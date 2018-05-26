d81f23ad88fa506bf0de542981fd19241dc42780
Vijayakumar Balakrishnan <vijayakumar_b@hotmail.com>
2018-05-26 20:46:45 +0530

    Initial version

---
+++ MultiplesOf3And5/MultiplesOf3And5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Project.Euler
+{
+    class MultiplesOf3And5
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            
+            int[] prod = new int[2];            
+
+            for (int i=3; i<1000; i++)
+            {
+                if ((i%3) == 0)
+                {
+                    sum += i;
+                } else if ((i%5) == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine("Sum of multiples of 3 and 5 where the product is less than 1000: {0}", sum);
+        }
+    }
+}
